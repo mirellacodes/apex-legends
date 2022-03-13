6dc81a08a720b8baed75c9ca58835008d0777801
Mirella <mirellacodes@gmail.com>
2022-03-13 16:49:29 +0000

    git init & create apex legend api

---
+++ data.ts
@@ -0,0 +1,81 @@
+const data = [
+    {  character: "BloodHound",
+    realName: "Unknown",
+    age: "Unknown",
+    homeWorld: "Talos",
+    tacticalAbility: "Eyes of the Allfather",
+    passiveAbility: "Tracker",
+    ultimateAbility: "Beast of the Hunt"
+
+},
+{
+    character: "Gibraltar",
+    realName:"Makoa Gibraltar",
+    age: 30,
+    homeWorld: "Solace",
+    tacticalAbility:"Dome of Protection",
+    passiveAbility: "Gun Shield",
+    ultimateAbility: "Defensive Bombardment"
+},
+{
+    character: "Lifeline",
+    realName: "	Ajay Che",
+    age: 24,
+    homeWorld: "Psmathe",
+    tacticalAbility: "D.O.C Heal Drone",
+    passiveAbility: "Combat Medic",
+    ultimateAbility:  "Care Package"
+    
+ 
+},
+
+{
+    character:,
+    realName: ,
+    age: ,
+    homeWorld: ,
+    tacticalAbility: ,
+    passiveAbility: ,
+    ultimateAbility: 
+    
+ 
+},
+
+{
+    character:,
+    realName: ,
+    age: ,
+    homeWorld: ,
+    tacticalAbility: ,
+    passiveAbility: ,
+    ultimateAbility: 
+    
+ 
+},
+
+{
+    character:,
+    realName: ,
+    age: ,
+    homeWorld: ,
+    tacticalAbility: ,
+    passiveAbility: ,
+    ultimateAbility: 
+    
+ 
+},
+
+{
+    character:,
+    realName: ,
+    age: ,
+    homeWorld: ,
+    tacticalAbility: ,
+    passiveAbility: ,
+    ultimateAbility: 
+    
+ 
+},
+]
+
+export default data;
---
+++ routes/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import * as cors from "cors";
+const app = express();
+app.use(cors());
+
+export default app;
